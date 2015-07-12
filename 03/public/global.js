052634d854ae64140d4fdd333e931c7dbd13ff65
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-12 16:19:46 -0500

    Finished Ajax 03

--- 03/public/global.js
+++ 03/public/global.js
@@ -1,20 +1,19 @@
-var req = new XMLHttpRequest();
-
-req.open("get", "/path1");
-
-req.addEventListener("load", function(){
-  alert(this.response);
-});
-
-req.send();
-
 //  ---------------------------------------------------------------------------
 
 var myLink = document.getElementById("clicker");
 
 var handleTheClick = function(event){
+  var thisLink = this;
   // ?????????
+  var req = new XMLHttpRequest();
 
+  req.open("get", thisLink.getAttribute("href"));
+
+  req.addEventListener("load", function(){
+    thisLink.innerText = this.response;
+  });
+
+  req.send();
   // Prevent the link from refreshing the DOM.
   event.preventDefault();
 }
